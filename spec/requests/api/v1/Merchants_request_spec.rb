195ab6ca35a62bef75fd1cb54549216ec6346a2e
Michael Abbott <michael@mjabbottdesign.com>
2021-09-05 21:53:29 -0700

    Add return merchant detials by id/ show test

--- spec/requests/api/v1/Merchants_request_spec.rb
+++ spec/requests/api/v1/Merchants_request_spec.rb
@@ -24,4 +24,27 @@ describe 'Merchants API' do
       expect(merchant[:attributes][:name]).to be_a(String)
     end
   end
+
+  it 'sends the data of a single merchant by id' do
+    create_list(:merchant, 40)
+
+    get "/api/v1/merchants/#{Merchant.first.id}"
+
+    expect(response).to be_successful
+
+    merchants = JSON.parse(response.body, symbolize_names: true)
+
+    expect(merchants.count).to eq(1)
+
+    expect(merchants[:data]).to have_key(:id)
+    expect(merchants[:data][:id]).to be_a(String)
+    expect(merchants[:data][:id]).to eq(Merchant.first.id.to_s)
+    expect(merchants[:data]).to have_key(:type)
+    expect(merchants[:data][:type]).to be_a(String)
+    expect(merchants[:data]).to have_key(:attributes)
+    expect(merchants[:data][:attributes]).to be_a(Hash)
+    expect(merchants[:data][:attributes]).to have_key(:name)
+    expect(merchants[:data][:attributes][:name]).to be_a(String)
+    expect(merchants[:data][:attributes][:name]).to eq(Merchant.first.name)
+  end
 end
