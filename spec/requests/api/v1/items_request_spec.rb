cae994f5fb50405cf267fb2201e69d86f5a7c8b8
Michael Abbott <michael@mjabbottdesign.com>
2021-09-07 21:52:59 -0700

    Create Items API request test; find all items name search test

---
+++ spec/requests/api/v1/items_request_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+describe 'Items API' do
+  describe 'Find all items' do
+    before :each do
+      @merchant = create(:merchant)
+      @item1 = create(:item, name: 'Super stuff', merchant: @merchant)
+      @item2 = create(:item, name: 'Stuff super', merchant: @merchant)
+      @item3 = create(:item, name: 'Superlicious', merchant: @merchant)
+      @item4 = create(:item, name: 'Lame', merchant: @merchant)
+      @item5 = create(:item, name: 'Lame too', merchant: @merchant)
+    end
+
+    it 'sends back all items, based on a name query' do
+      param = 'super'
+      get "/api/v1/items/find_all?name=#{param}"
+      items = JSON.parse(response.body, symbolize_names: true)
+
+      expect(items[:data].count).to eq(3)
+      expect(items[:data].any? {|x| x[:id] == @item1.id.to_s}).to eq(true)
+      expect(items[:data].any? {|x| x[:id] == @item2.id.to_s}).to eq(true)
+      expect(items[:data].any? {|x| x[:id] == @item3.id.to_s}).to eq(true)
+      expect(items[:data].any? {|x| x[:id] == @item4.id.to_s}).to eq(false)
+      expect(items[:data].any? {|x| x[:id] == @item5.id.to_s}).to eq(false)
+    end
+  end
+end
