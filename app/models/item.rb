524c3bb3726858a5d715387d675ee01681ca6b86
Michael Abbott <michael@mjabbottdesign.com>
2021-09-07 21:58:45 -0700

    Add Item ::find_all_items

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,8 @@
 class Item < ApplicationRecord
   belongs_to :merchant
+
+  def self.find_all_items(name_query)
+    where("name ~* ?", name_query).order(:name)
+    # where("name iLIKE ?", "%#{name_query}%").order(:name).take
+  end
 end
