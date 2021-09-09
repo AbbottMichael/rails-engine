807171a519c0ea45c53373f06b13aac02be39710
Michael Abbott <michael@mjabbottdesign.com>
2021-09-09 14:20:53 -0700

    Add merchants/most_items search#index route

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
       end
       namespace :merchants do
         get '/find', to: 'search#show'
+        get '/most_items', to: 'search#index'
       end
       resources :merchants, only: [:index, :show] do
         resources :items, only: [:index]
