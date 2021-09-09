8859c79de3564e9717f3288b0ccf502bcd8edf76
Michael Abbott <michael@mjabbottdesign.com>
2021-09-09 12:14:42 -0700

    Refactor items search controller

--- app/controllers/api/v1/items/search_controller.rb
+++ app/controllers/api/v1/items/search_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::Items::SearchController < ApplicationController
 
   def index
-    return (render :status => :bad_request) if params[:name] == nil || params[:name].empty?
+    return (render status: :bad_request) if params[:name] == nil || params[:name].empty?
     render json: ItemSerializer.new(Item.find_all_items(params[:name]))
   end
 end
