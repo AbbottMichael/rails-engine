870db8580982ff6417c20ee264bd7cccb6cb93a0
Michael Abbott <michael@mjabbottdesign.com>
2021-09-09 12:14:06 -0700

    Refactor merchants search controller

--- app/controllers/api/v1/merchants/search_controller.rb
+++ app/controllers/api/v1/merchants/search_controller.rb
@@ -2,6 +2,7 @@ class Api::V1::Merchants::SearchController < ApplicationController
 
   def show
     return (render status: :bad_request) if (params[:name] == nil) || (params[:name].empty?)
-    render json: MerchantSerializer.new(Merchant.find_one(params[:name]))
+    merchant_json = MerchantSerializer.new(Merchant.find_one(params[:name]))
+    (merchant_json.to_json.include?("null")) ? (render json: { data:{}}) : (render json: merchant_json)
   end
 end
--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -10,5 +10,4 @@ class Api::V1::MerchantsController < ApplicationController
   def show
     render json: MerchantSerializer.new(Merchant.find(params[:id]))
   end
-
 end
