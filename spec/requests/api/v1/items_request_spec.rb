7d39f6293b63bf48e4b57987489e4f3ee77fab3a
Michael Abbott <michael@mjabbottdesign.com>
2021-09-09 18:06:40 -0700

    Add item most revenue request test

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -24,4 +24,57 @@ describe 'Items API' do
       expect(items[:data].any? {|item| item[:id] == @item5.id.to_s}).to eq(false)
     end
   end
+
+  describe 'items ranked by descending revenue' do
+    before :each do
+      @customer1 = create(:customer)
+      @merchant1 = create(:merchant)
+      @item1 = create(:item, merchant_id: @merchant1.id)
+      @invoice1 = create(:invoice, merchant_id: @merchant1.id, customer_id: @customer1.id)
+      create(:invoice_item, item_id: @item1.id, invoice_id: @invoice1.id)
+      create(:transaction, invoice_id: @invoice1.id)
+      @invoice2 = create(:invoice, merchant_id: @merchant1.id, customer_id: @customer1.id)
+      create(:invoice_item, item_id: @item1.id, invoice_id: @invoice2.id)
+      create(:transaction, invoice_id: @invoice2.id)
+      @merchant2 = create(:merchant)
+      @item2 = create(:item, merchant_id: @merchant2.id)
+      @invoice3 = create(:invoice, merchant_id: @merchant2.id, customer_id: @customer1.id)
+      create(:invoice_item, quantity: 200, item_id: @item2.id, invoice_id: @invoice3.id)
+      create(:transaction, invoice_id: @invoice3.id)
+      @invoice4 = create(:invoice, merchant_id: @merchant2.id, customer_id: @customer1.id)
+      create(:invoice_item, item_id: @item2.id, invoice_id: @invoice4.id)
+      create(:transaction, invoice_id: @invoice4.id)
+    end
+
+    it 'sends back a chosen number of items ranked by revenue in descending order' do
+      get "/api/v1/revenue/items?quantity=1"
+      items = JSON.parse(response.body, symbolize_names: true)
+
+      expect(items[:data].count).to eq(1)
+
+      get "/api/v1/revenue/items?quantity=2"
+      items = JSON.parse(response.body, symbolize_names: true)
+
+      expect(items[:data].count).to eq(2)
+
+      items[:data].each do |item|
+        expect(item).to have_key(:id)
+        expect(item[:id]).to be_a(String)
+        expect(item).to have_key(:type)
+        expect(item[:type]).to eq('item_revenue')
+        expect(item).to have_key(:attributes)
+        expect(item[:attributes]).to be_a(Hash)
+        expect(item[:attributes]).to have_key(:name)
+        expect(item[:attributes][:name]).to be_a(String)
+        expect(item[:attributes]).to have_key(:description)
+        expect(item[:attributes][:description]).to be_a(String)
+        expect(item[:attributes]).to have_key(:unit_price)
+        expect(item[:attributes][:unit_price]).to be_a(Float)
+        expect(item[:attributes]).to have_key(:merchant_id)
+        expect(item[:attributes][:merchant_id]).to be_an(Integer)
+        expect(item[:attributes]).to have_key(:revenue)
+        expect(item[:attributes][:revenue]).to be_a(Float)
+      end
+    end
+  end
 end
