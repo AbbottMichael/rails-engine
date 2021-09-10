761c4da016f8ada64b62f7da888e3e4075d29ad6
Michael Abbott <michael@mjabbottdesign.com>
2021-09-09 21:14:30 -0700

    Add sends a list of items request test

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -1,6 +1,59 @@
 require 'rails_helper'
 
 describe 'Items API' do
+  it 'sends a list of items' do
+    create(:merchant, id: 1)
+    create(:merchant, id: 2)
+    create_list(:item, 25, merchant_id: 1)
+    create_list(:item, 25, merchant_id: 2)
+
+    get '/api/v1/items'
+
+    expect(response).to be_successful
+
+    items = JSON.parse(response.body, symbolize_names: true)
+
+    expect(items[:data].count).to eq(20)
+
+    items[:data].each do |item|
+      expect(item).to have_key(:id)
+      expect(item[:id]).to be_a(String)
+      expect(item).to have_key(:type)
+      expect(item[:type]).to eq('item')
+      expect(item).to have_key(:attributes)
+      expect(item[:attributes]).to be_a(Hash)
+      expect(item[:attributes]).to have_key(:name)
+      expect(item[:attributes][:name]).to be_a(String)
+      expect(item[:attributes]).to have_key(:description)
+      expect(item[:attributes][:description]).to be_a(String)
+      expect(item[:attributes]).to have_key(:unit_price)
+      expect(item[:attributes][:unit_price]).to be_a(Float)
+    end
+  end
+
+  xit 'sends the data of a single merchant by id' do
+    create_list(:merchant, 40)
+
+    get "/api/v1/merchants/#{Merchant.first.id}"
+
+    expect(response).to be_successful
+
+    merchants = JSON.parse(response.body, symbolize_names: true)
+
+    expect(merchants.count).to eq(1)
+
+    expect(merchants[:data]).to have_key(:id)
+    expect(merchants[:data][:id]).to be_a(String)
+    expect(merchants[:data][:id]).to eq(Merchant.first.id.to_s)
+    expect(merchants[:data]).to have_key(:type)
+    expect(merchants[:data][:type]).to be_a(String)
+    expect(merchants[:data]).to have_key(:attributes)
+    expect(merchants[:data][:attributes]).to be_a(Hash)
+    expect(merchants[:data][:attributes]).to have_key(:name)
+    expect(merchants[:data][:attributes][:name]).to be_a(String)
+    expect(merchants[:data][:attributes][:name]).to eq(Merchant.first.name)
+  end
+
   describe 'Find all items' do
     before :each do
       @merchant = create(:merchant)
