c3165f17f52fb9e79c4a078a74b65e5071931b60
Michael Abbott <michael@mjabbottdesign.com>
2021-09-09 21:14:51 -0700

    Refactor routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,17 +2,22 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
+      
       namespace :items do
         get '/find_all', to: 'search#index'
+        get '/find_all', to: 'search#index'
       end
+
       namespace :merchants do
         get '/find',       to: 'search#show'
         get '/most_items', to: 'search#index'
+        get ':merchant_id/items',      to: 'items#index'
       end
-      resources :merchants, only: [:index, :show] do
-        resources :items, only: [:index]
-      end
+
+      resources :merchants, only: [:index, :show]
+
       resources :items, only: [:index]
+
       namespace :revenue do
         get '/merchants',     to: 'search_merchant#index'
         get '/merchants/:id', to: 'search_merchant#show'
