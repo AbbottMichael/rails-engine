7f3da431d069fcb237efadba3d1b2eefecaf4245
Michael Abbott <michael@mjabbottdesign.com>
2021-09-09 21:16:54 -0700

    Refactor ItemsController #index

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,10 +1,9 @@
 class Api::V1::ItemsController < ApplicationController
 
   def index
-    # per_page = params.fetch(:per_page, 20).to_i
-    # page = params.fetch(:page, 0).to_i
-    # page = page - 1 unless page == 0
-    # render json: MerchantSerializer.new(Merchant.offset(page * per_page).limit(per_page))
-    render json: ItemSerializer.new(Merchant.find(params[:merchant_id].to_i).items)
+    per_page = params.fetch(:per_page, 20).to_i
+    page = params.fetch(:page, 0).to_i
+    page = page - 1 unless page == 0
+    render json: ItemSerializer.new(Item.offset(page * per_page).limit(per_page))
   end
 end
