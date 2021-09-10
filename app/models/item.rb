ad3345d0ade82edb1c9da716e16a721669486cad
Michael Abbott <michael@mjabbottdesign.com>
2021-09-09 18:08:32 -0700

    Add Item ::total_revenue_desc

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,4 +7,16 @@ class Item < ApplicationRecord
     where("name ~* ?", name_query).order(:name)
     # where("name iLIKE ?", "%#{name_query}%").order(:name).take
   end
+
+  def self.total_revenue_desc(item_count)
+    # joins(invoices: [:transactions, :invoice_items])
+    Item.joins(invoice_items: { invoice: :transactions })
+    .where('invoices.status = ?', 'shipped')
+    .where('transactions.result = ?', 'success')
+    .select('sum(invoice_items.quantity * invoice_items.unit_price) AS revenue')
+    .select('items.id', 'items.name', 'items.description', 'items.unit_price', 'merchant_id')
+    .group('items.id')
+    .order(revenue: :desc)
+    .limit(item_count)
+  end
 end
