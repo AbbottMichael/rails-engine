f65046f783fade0fce65e003287e388a68e7a1be
Michael Abbott <michael@mjabbottdesign.com>
2021-09-09 22:06:37 -0700

    Refactor MerchantController #pagination

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,13 +1,18 @@
 class Api::V1::MerchantsController < ApplicationController
 
   def index
-    per_page = params.fetch(:per_page, 20).to_i
-    page = params.fetch(:page, 0).to_i
-    page = page - 1 unless page == 0
-    render json: MerchantSerializer.new(Merchant.offset(page * per_page).limit(per_page))
+    pagination
+    render json: MerchantSerializer.new(Merchant.offset(@page * @per_page).limit(@per_page))
   end
 
   def show
     render json: MerchantSerializer.new(Merchant.find(params[:id]))
+  end
+
+  private
+  def pagination
+    @per_page = params.fetch(:per_page, 20).to_i
+    @page = params.fetch(:page, 0).to_i
+    @page = @page - 1 unless @page == 0
   end
 end
