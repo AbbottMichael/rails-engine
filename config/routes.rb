920d35aee457c0b374d4c189903a1953abf30885
Michael Abbott <michael@mjabbottdesign.com>
2021-09-09 19:18:25 -0700

    Add revenue/merchants/:id #search_merchant#show route

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
         get '/find_all', to: 'search#index'
       end
       namespace :merchants do
-        get '/find', to: 'search#show'
+        get '/find',       to: 'search#show'
         get '/most_items', to: 'search#index'
       end
       resources :merchants, only: [:index, :show] do
@@ -14,8 +14,9 @@ Rails.application.routes.draw do
       end
       resources :items, only: [:index]
       namespace :revenue do
-        get '/merchants', to: 'search_merchant#index'
-        get '/items', to: 'search_item#index'
+        get '/merchants',     to: 'search_merchant#index'
+        get '/merchants/:id', to: 'search_merchant#show'
+        get '/items',         to: 'search_item#index'
       end
     end
   end
